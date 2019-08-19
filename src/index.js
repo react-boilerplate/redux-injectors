b6a019d1b01f8461076da61d5acd5bd5d930483e
Ben <benlorantfy@gmail.com>
2019-08-19 14:59:04 -0400

    Remove checkStore from exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as checkStore } from "./checkStore";
 export { forceReducerReload } from "./forceReducerReload";
 export { default as injectReducer, useInjectReducer } from "./injectReducer";
 export { default as injectSaga, useInjectSaga } from "./injectSaga";
